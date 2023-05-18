763e025d598ef320edc95fc1bf893258f7f238e7
stas-shr <stasshr@gmail.com>
2023-05-18 18:50:18 +0300

    Square done

--- Inertial_Square/ViewController.swift
+++ Inertial_Square/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var square: UIView = {
+        let square = UIView()
+        square.frame.size = CGSize(width: 100, height: 100)
+        square.center = self.view.center
+        square.translatesAutoresizingMaskIntoConstraints = false
+        square.backgroundColor = .systemBlue
+        square.layer.cornerRadius = 10
+        return square
+    }()
+    
+    lazy var animatior = UIDynamicAnimator(referenceView: view)
+    
+    lazy var snapBehavior = UISnapBehavior(item: square, snapTo: square.center)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(square)
+        
+        let move = UITapGestureRecognizer(target: self, action: #selector(moveAction))
+        view.addGestureRecognizer(move)
+        snapBehavior.damping = 2
+        animatior.addBehavior(snapBehavior)
     }
-
-
+    
+    @objc private func moveAction(_ recognizer: UITapGestureRecognizer) {
+        snapBehavior.snapPoint = recognizer.location(in: view)
+    }
 }
 
